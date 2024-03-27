44de3d4c5701214a7d83b55007f451ba2619eb1a
nataliat21 <natalia.torozco21@gmail.com>
2024-03-27 16:08:05 -0500

    The main component was created and imported into the app component

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-main',
+  selector: 'main-component',
   standalone: true,
   imports: [],
   templateUrl: './main.component.html',

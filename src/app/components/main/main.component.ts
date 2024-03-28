51dc3dca8eb5ea0a225794371d876c49f6cbe002
nataliat21 <natalia.torozco21@gmail.com>
2024-03-27 19:13:44 -0500

    The promotional flight section was added to the main component, and it was adapted to mobile devices

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -8,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+  clickButton: boolean = false;
+  clickButtonDestinations: boolean = false;
+
+  onClick(): void {
+    this.clickButton = true;
+    setTimeout(() => {
+      this.clickButton = false;
+    }, 700);
+  }
+
+  onClickViewDestinations(): void {
+    this.clickButtonDestinations = true;
+    setTimeout(() => {
+      this.clickButtonDestinations = false;
+    }, 700);
+  }
+
+  
 
 }

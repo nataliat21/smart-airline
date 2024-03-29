eb09607fab8b42453c74094ea5067e3081190027
nataliat21 <natalia.torozco21@gmail.com>
2024-03-28 21:35:04 -0500

    Additional comments are added to components

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,22 +10,25 @@ import { Component, Output, EventEmitter } from '@angular/core';
 export class HeaderComponent {
 
   @Output() menuToggled = new EventEmitter<boolean>();
+
   openMenu: boolean = false;
   searchButtonMobile: boolean = false;
   searchButtonDesktop: boolean = false;
 
+  //Method that is responsible for toggling the visibility of the menu
   toggleMenu() {
     this.openMenu = !this.openMenu;
     this.menuToggled.emit(this.openMenu);
   }
 
+  //Method that is responsible for handling an event that occurs when a search is clicked on the mobile device.
   onClickSearchMobile(): void {
     this.searchButtonMobile = true;
     setTimeout(() => {
       this.searchButtonMobile = false;
     }, 700);
   }
-
+//Method that is responsible for handling an event that occurs when a search is clicked on the desktop.
   onClickSearchDesktop(): void {
     this.searchButtonDesktop = true;
     setTimeout(() => {
